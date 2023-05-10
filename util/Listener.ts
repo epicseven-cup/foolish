c360a54f521ca85981e9d371b424fc2edab3930f
epicseven-cup <jllin3@buffalo.edu>
2023-05-10 11:29:37 -0400

    adding custom equst and resond clases

--- util/Listener.ts
+++ util/Listener.ts
@@ -1,3 +1,14 @@
 class FoolishListener {
-    
+    _socket = null
+    _port = "4000"
+    _handler = (request:FoolishRequest) => {return;}
+    constructor(port, handler){
+        this._port = port
+        this._socket = Deno.listen({ port: port, transport: "tcp"})
+        this._handler = handler
+    }
+
+    Accpect(): FoolishRespond{
+
+    }
 }
